cba3f6ea635d0e381da99ddbb8be117bcb3d1aef
baowp <122515909@qq.com>
2013-10-25 20:53:56 +0800

    a

---
+++ web/src/main/java/cn/com/chinabank/app2/controller/DubboSampleController.java
@@ -0,0 +1,39 @@
+/**
+ * Project: application2.web
+ * 
+ * File Created at Oct 25, 2013
+ * $Id$ Corporation
+ * 
+ * Copyright 2013-2019 Chinabank.com.cn Corporation Limited.
+ * All rights reserved.
+ */
+package cn.com.chinabank.app2.controller;
+
+import java.io.Serializable;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.com.chinabank.shared.service.DubboSampleService;
+
+/**
+ * @author baowp
+ *
+ */
+@Controller
+@RequestMapping("dubbo")
+public class DubboSampleController {
+
+	@Resource
+	private DubboSampleService dubboSampleService;
+
+	@ResponseBody
+	@RequestMapping(value = "/entry")
+	public Serializable entry(String name) {
+
+		return dubboSampleService.execute(name);
+	}
+}
