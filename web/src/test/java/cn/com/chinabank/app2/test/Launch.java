670a74d61d5fe746a1691c45f7f2d164abe8c376
baowp <122515909@qq.com>
2013-11-07 22:16:04 +0800

    a

---
+++ web/src/test/java/cn/com/chinabank/app2/test/Launch.java
@@ -0,0 +1,16 @@
+package cn.com.chinabank.app2.test;
+
+import cn.com.chinabank.shared.launch.Main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: baowp
+ * Date: 11/7/13
+ * Time: 10:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Launch {
+    public static void main(String args[]) {
+        cn.com.chinabank.shared.launch.Main.main(new String[]{"spring","jetty"});
+    }
+}
