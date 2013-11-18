7b178d916933933e220660ab95dc220b869ece5b
baowp <122515909@qq.com>
2013-11-18 09:34:46 +0800

    a

--- web/src/main/java/cn/com/chinabank/app2/controller/DubboSampleController.java
+++ web/src/main/java/cn/com/chinabank/app2/controller/DubboSampleController.java
@@ -1,40 +1,58 @@
 /**
  * Project: application2.web
- * 
+ *
  * File Created at Oct 25, 2013
  * $Id$ Corporation
- * 
+ *
  * Copyright 2013-2019 Chinabank.com.cn Corporation Limited.
  * All rights reserved.
  */
 package cn.com.chinabank.app2.controller;
 
-import java.io.Serializable;
-
-import javax.annotation.Resource;
-
+import cn.com.chinabank.shared.listener.CallbackListener;
+import cn.com.chinabank.shared.service.CallbackService;
+import cn.com.chinabank.shared.service.DubboSampleService;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import cn.com.chinabank.shared.service.DubboSampleService;
+import javax.annotation.Resource;
+import java.io.Serializable;
 
 
 /**
  * @author baowp
- *
  */
 @Controller
 @RequestMapping("dubbo")
 public class DubboSampleController {
 
-	@Resource
-	private DubboSampleService dubboSampleService;
+    private final org.apache.log4j.Logger logger = org.apache.log4j.Logger
+            .getLogger(getClass());
 
-	@ResponseBody
-	@RequestMapping(value = "/entry")
-	public Serializable entry(String name) {
-		return dubboSampleService.execute(name);
-	}
+    @Resource
+    private DubboSampleService dubboSampleService;
+    @Resource
+    private CallbackService callbackService;
+
+    @ResponseBody
+    @RequestMapping(value = "/entry")
+    public Serializable entry(String name) {
+        return dubboSampleService.execute(name);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/examine")
+    public Object examine() {
+       Object obj= callbackService.addListener("foo.bar", new CallbackListener(){
+            public void changed(String msg) {
+                System.out.println("callback1:" + msg);
+            }
+        });
+        logger.info(obj);
+        return obj;
+    }
+
 
 }
